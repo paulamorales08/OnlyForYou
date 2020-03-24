7e79b3fb511d37186b23de859ab5ecd291becb82
Paula Morales <paula.morales@empresariales.com.co>
2020-03-24 09:16:11 -0500

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+ 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+/* import { ServiciosComponent } from './components/servicios/servicios.component'; */
+
+const appRoutes: Routes = [
+  /* {path: 'servicios',component: ServiciosComponent}, */
+  {
+    path:'',
+    pathMatch: 'full', //ruta inicial
+    redirectTo: 'body',
+  }
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarrouselComponent,
+    FooterComponent,
+    BodyComponent,
+    /* ServiciosComponent, */
+    
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  FormsModule,
+  RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
